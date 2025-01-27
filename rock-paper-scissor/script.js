4cb9fc890110eb5f538d79013a60430d9310e0b5
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-01-27 23:23:07 +0700

    add score

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -3,6 +3,10 @@ const choiceBtns = document.querySelectorAll(".choice-btn")
 const playerChoiceText = document.querySelector(".player-choice-text")
 const cpuChoiceText = document.querySelector(".cpu-choice-text")
 
+const scoreWonText = document.querySelector(".score-won-text")
+const scoreDrawText = document.querySelector(".score-draw-text")
+const scoreLostText = document.querySelector(".score-lost-text")
+
 const gameTitle = document.querySelector(".game-title")
 
 let playerResultValue = ""
